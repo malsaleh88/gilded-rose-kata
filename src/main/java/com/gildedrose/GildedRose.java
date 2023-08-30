5449868df2bba720bfe925d952143dac7e85ed72
malsaleh <mohammad.alsaleh@uclouvain.be>
2023-08-30 14:44:48 +0200

    Enhance GildedRose Class
    - Improved code readability.
    - Added comments for better understanding.
    - Ensured code consistency.
    
    This enhances code quality for easier maintenance.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,56 +7,96 @@ class GildedRose {
         this.items = items;
     }
 
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item:items) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+             item.sellIn--;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+             if (item.name.equals("Aged Brie")){
+                 increaseTheQuality(item);
+             }else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+                 qualityOfBackstage(item);
+             }else if (item.name.equals("Conjured Mana Cake")) {
+                 decreaseTheQuality(item, 2);
+             } else {
+                 decreaseTheQuality(item);
+             }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+        }
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+    /**
+     * Decrease the quality of an item. If the item is past its sell-by date, decrease the quality twice as fast.
+     *
+     * @param item The item whose quality is being decreased.
+     * @param fall The amount by which to decrease the quality.
+     */
+    private void decreaseTheQuality(Item item,int fall) {
+        for (int i = 0; i < fall; i++) {
+            decreaseTheQuality(item);
+        }
+    }
+
+    /**
+     * Increase the quality of an item.
+     *
+     * @param item The item to increase the quality of.
+     * @param rise The amount by which to increase the quality.
+     */
+    private void increaseTheQuality(Item item,int rise) {
+        for (int i = 0; i < rise; i++) {
+            increaseTheQuality(item);
+        }
+    }
+
+    /**
+     * Increase the quality of an item by one, but only if the quality is below 50.
+     *
+     * @param item The item to increase the quality of.
+     */
+    private void increaseTheQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    /**
+     * Decreases the quality of an item. If the item is past its sell-by date, the quality decreases faster.
+     *
+     * @param item The item to decrease the quality of.
+     */
+    private void decreaseTheQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality--;
             }
         }
     }
-}
+
+    /**
+     * Adjust the quality of a backstage pass item based on its sell-in value. If the sell-in is negative (i.e., the event
+     * has passed), set the quality to 0. Otherwise, increase the quality: by 3 if sell-in is 5 days or less, by 2 if less
+     * than 10 days, or by 1 otherwise.
+     *
+     * @param item The backstage pass item to adjust.
+     */
+    private void qualityOfBackstage(Item item) {
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        } else if (item.sellIn <= 5) {
+            increaseTheQuality(item, 3);
+        } else if (item.sellIn <= 10) {
+            increaseTheQuality(item, 2);
+        } else {
+            increaseTheQuality(item);
+        }
+    }
+
+
+    }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,80 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testNormalItemQualityDecrease() {
+        Item[] items = new Item[] { new Item("Normal Item", 5, 10) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(9, app.items[0].quality);
     }
 
+    @Test
+    void testNormalItemQualityDecreaseAfterSellIn() {
+        Item[] items = new Item[] { new Item("Normal Item", -1, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    void testAgedBrieQualityIncrease() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
+    }
+
+    @Test
+    public void testFailingUpdateQuality() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 30) };
+        GildedRose app = new GildedRose(items);
+
+        // Call updateQuality once
+        app.updateQuality();
+
+        // This assertion will fail because the quality of Aged Brie should increase, but we expect it to stay the same
+        assertEquals(31, items[0].quality);
+    }
+    @Test
+    void testAgedBrieQualityIncreaseAfterSellIn() {
+        Item[] items = new Item[] { new Item("Aged Brie", -1, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
+    }
+
+    @Test
+    void testBackstagePassQualityIncrease() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
+    }
+
+    @Test
+    void testBackstagePassQualityIncreaseMore() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(13, app.items[0].quality);
+    }
+
+    @Test
+    void testBackstagePassQualityZeroAfterSellIn() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void testConjuredItemQualityDecrease() {
+        Item[] items = new Item[] { new Item("Conjured Mana Cake", 5, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+    }
 }
